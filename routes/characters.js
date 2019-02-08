afd51a03086eef76dbfc5fc5d759cfcebd0b6c0e
Wing <eamonn.kearns@so-4pt.net>
2019-02-08 12:30:08 +0000

    Had to add use strict everywhere.
    Also started working on the Discord Bot

--- routes/characters.js
+++ routes/characters.js
@@ -8,5 +8,6 @@ router.get('/', async(controller.indexAction));
 router.post('/new', async(controller.newCharacterAction));
 router.get('/fetch/:reference', async(controller.fetchAction));
 router.post('/save', async(controller.saveCharacterAction));
+router.post('/generateAPIKey', async(controller.generateAPIKey));
 
 module.exports = router;
